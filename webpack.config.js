edaac4ac27c27b4249a9e76553bb13e5dda9dd40
wyuye <wyuye_beself@qq.com>
2018-02-04 09:47:53 +0800

    no mes

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js', // 项目入口
-  // entry: './src/lib/index.js',  // 修改项目入口
+  // entry: './src/main.js', // 项目入口
+  entry: './src/lib/index.js',  // 修改项目入口
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     // filename: 'build.js',
     filename: 'vue-pdf-viewer.js',
+    chunkFilename: 'js/vue-pdf-viewer.app.js',
     library: 'VuePdfViewer', // library指定的就是你使用require时的模块名，这里便是require("PayKeyboard")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
